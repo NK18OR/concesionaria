25a6e859c1b0593c85f7ded90cc994b9aab37261
NK18OR <axel.leonelromano@gmail.com>
2021-08-28 10:36:56 -0300

    agregue la funcionalidad autosParaLaVenta y autosNuevos

--- concesionaria.js
+++ concesionaria.js
@@ -7,20 +7,29 @@ let concesionaria = {
       for(let i = 0 ; i <autos.length; i++){
       if(patentes === autos[i].patente){
          return autos[i];
-   } 
+      }
    }return null;
    },
    venderAuto: function(patente){
-      let auto = this.buscarAuto(patente);
+      auto = this.buscarAuto(patente);
       auto.vendido = true;
    },
    autosParaLaVenta:function(){
-      autosParaVender = autos.filter(function (vendidos) {
-         return vendidos.vendido == false
+      autosParaVender = this.autos.filter(function (vendidos) {
+         return vendidos.vendido == false;
       })
-      return autosParaVender
+      return autosParaVender;
+   },
+   autosNuevos: function(){
+      autoNuevo = this.autosParaLaVenta().filter(function(elemento){
+         return elemento.km < 100
+      })
+      return autoNuevo
+   },
+   listaDeVentas: 
+   
    }
-   }
-console.log(concesionaria.venderAuto("JJK116"));
-console.log(concesionaria.autos);
-console.log(concesionaria.autosParaLaVenta());
+//console.log(concesionaria.venderAuto("JJK116"));
+//console.log(concesionaria.autos);
+//console.log(concesionaria.autosParaLaVenta());
+//=console.log(concesionaria.autosNuevos());
